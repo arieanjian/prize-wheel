f2de50c97cec253aeb13298813b2837eec63b6df
Ariean Jian <arieanjian@gmail.com>
2024-08-25 21:00:48 +0800

    docs: 修改檔案

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+// style
+import "./styles/index.css";
+import "./styles/antdCust.css";
+
+import App from "./App";
+import ReactDOM from "react-dom/client";
+
+// Add type declaration for the imported module
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
--- src/main.tsx
+++
@@ -1,7 +0,0 @@
-import ReactDOM from "react-dom/client";
-import App from "./App";
-// style
-import "./styles/index.css";
-import "./styles/antdCust.css";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
